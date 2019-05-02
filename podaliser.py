5f3d854b42ece3f21e8e9f8c9dacf1ef87aea42d
MDornacher <manuel.dornacher@gmail.com>
2019-05-02 19:22:16 +0200

    some progress

--- podaliser.py
+++ podaliser.py
@@ -1,22 +1,51 @@
 import opml
-from pyPodcastParser.Podcast import Podcast
 import requests
+from pyPodcastParser.Podcast import Podcast
+from podcast_episodes import MyPodcast, MyEpisode
 
-outline = opml.parse('feed.opml')
-f = open("output.txt", "w")
 
-for shows in outline:
-    podcast_url = shows.xmlUrl
-    # print(podcast_url)
+def get_duration_in_seconds(duration):
+    duration_list = duration.split(":")
+    hours = 0
+    mins = 0
+    secs = 0
+    if len(duration_list) == 3:
+        hours, mins, secs = duration_list
+    elif len(duration_list) == 2:
+        mins, secs = duration_list
+    elif len(duration_list) == 1:
+        secs = duration
+    duration = int(hours) * 3600 + int(mins) * 60 + int(secs)
 
-    response = requests.get(podcast_url)
-    podcast = Podcast(response.content)
+    return duration
 
-    print(podcast.title)
-    f.write(podcast.title)
-    #print(len(podcast.items), "episodes found!")
 
-    for episode in podcast.items:
-        f.write(str(episode.published_date) + "\t" + str(episode.title) + "\t" + str(episode.itunes_duration))
+if __name__ == "__main__":
+    outline = opml.parse('feed.opml')
+    f = open("output_filtered", "w")
 
-f.close()
+    podcast_list = []
+    for shows in outline:
+        podcast_url = shows.xmlUrl
+        response = requests.get(podcast_url)
+        podcast = Podcast(response.content)
+
+        podcast_list.append(MyPodcast(podcast.title))
+        print(podcast.title)
+        f.write("%s\n" % podcast.title)
+
+        for episode in podcast.items:
+            # print(episode.title.encode("utf-8"))
+            duration = episode.itunes_duration
+            if duration is not None:
+                weekday, day, month, year, *_ = episode.published_date.replace(",", "").split()
+                # TODO: convert date to calendar week number and use it as filter and norm factor
+                if month not in ["Feb", "Mar", "Apr"]:
+                    continue
+                duration = get_duration_in_seconds(duration)
+
+                new_episode = MyEpisode(weekday, day, month, year, str(duration))
+                podcast_list[-1].add_episode(new_episode)
+                f.write("%s\n" % duration)
+
+# TODO: stacked histogram with mean week time
---
+++ podcast_episodes.py
@@ -0,0 +1,17 @@
+
+class MyEpisode:
+    def __init__(self, weekday, day, month, year, duration):
+        self.weekday = weekday
+        self.day = day
+        self.month = month
+        self.year = year
+        self.duration = duration
+
+
+class MyPodcast:
+    def __init__(self, title):
+        self.title = title
+        self.episodes = []
+
+    def add_episode(self, episode):
+        self.episodes.append(episode)
